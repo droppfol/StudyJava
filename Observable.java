3699e7b2498a82d2fbcae5c1413107834172ac79
fol3IO <jd19rb@gmail.com>
2017-04-10 21:53:13 +0300

    Delete Observable.java

--- Observable.java
+++
@@ -1,8 +0,0 @@
-package spies;
-
-public interface Observable
-{
-    void addObserver(Observer observer);
-    void removeObserver(Observer observer);
-    void notifyObserver(Observer observer, Message message);
-}
